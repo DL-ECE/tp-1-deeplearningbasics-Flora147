78205368c19a3cec0c1f576683b7a682ab698a4a
Flora147 <36376325+Flora147@users.noreply.github.com>
2020-11-08 19:04:20 +0100

    Add files via upload

--- rapport.py
+++ rapport.py
@@ -97,7 +97,8 @@ plot_one_image(X_test, y_test , 250)
 
 # It's important to normalize the data before feeding it into the neural network
 def normalize_data(dataset: np.array) -> np.array:
-    normalized_dataset = (dataset - np.min(dataset)) / (np.max(dataset) - np.min(dataset))
+    #normalized_dataset = (dataset - np.min(dataset))/(np.max(dataset) - np.min(dataset))
+    normalized_dataset = dataset/255
     return normalized_dataset
 
 """It's also important to find a good representation of the target.
